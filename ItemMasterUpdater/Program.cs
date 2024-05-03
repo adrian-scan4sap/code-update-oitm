5628fa2b8d89c0c3087218dbe25746b921e6f0fa
adrian_scan4sap <adrian@scan4sap.com>
2024-05-03 13:08:10 +0100

    other properties

--- ItemMasterUpdater/Program.cs
+++ ItemMasterUpdater/Program.cs
@@ -65,22 +65,22 @@ namespace ItemMasterUpdater
                 {
                     if (!string.IsNullOrEmpty(itemData.Width))
                     {
-                        sapItem.SalesUnitWidth1 = double.Parse(itemData.Width);
+                        sapItem.SalesUnitWidth = double.Parse(itemData.Width);
                     }
 
                     if (!string.IsNullOrEmpty(itemData.Height))
                     {
-                        sapItem.SalesUnitHeight1 = double.Parse(itemData.Height);
+                        sapItem.SalesUnitHeight = double.Parse(itemData.Height);
                     }
 
                     if (!string.IsNullOrEmpty(itemData.Length))
                     {
-                        sapItem.SalesUnitLength1 = double.Parse(itemData.Length);
+                        sapItem.SalesUnitLength = double.Parse(itemData.Length);
                     }
 
                     if (!string.IsNullOrEmpty(itemData.Weight))
                     {
-                        sapItem.SalesUnitWeight1 = double.Parse(itemData.Weight);
+                        sapItem.SalesUnitWeight = double.Parse(itemData.Weight);
                     }
 
                     if (sapItem.Update() != 0)
